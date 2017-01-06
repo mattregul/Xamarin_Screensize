35b61968613445e8cb841437ccc28a746df78d16
Matt Regul <dev.matt@pachead.com>
2017-01-06 11:11:35 -0500

    first push

---
+++ screensize/screensize/screensize/screensize.Droid/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace screensize.Droid
+{
+    [Activity(Label = "screensize", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            
+            //###########################################################################################
+            //# Android #################################################################################
+            //###########################################################################################
+            
+            // Store off the device sizes, so we can access them within Xamarin Forms
+            //  Screen Width = WidthPixels / Density
+            //  Screen Height = HeightPixels / Density
+
+            App.DisplayScreenWidth  = (double)Resources.DisplayMetrics.WidthPixels / (double)Resources.DisplayMetrics.Density;
+            App.DisplayScreenHeight = (double)Resources.DisplayMetrics.HeightPixels / (double)Resources.DisplayMetrics.Density; 
+            App.DisplayScaleFactor  = (double)Resources.DisplayMetrics.Density;
+            
+            //###########################################################################################
+            //###########################################################################################
+            //###########################################################################################
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ screensize/screensize/screensize/screensize/App.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace screensize
+{
+    public class App : Application
+    {
+        public static double DisplayScreenWidth = 0f;
+        public static double DisplayScreenHeight = 0f;
+        public static double DisplayScaleFactor = 0f;
+
+        public App()
+        {
+
+            string ScreenDetails = Device.OS.ToString() + " Device Screen Size:\n" +
+                $"Width: {DisplayScreenWidth}\n" +
+                $"Height: {DisplayScreenHeight}\n" +
+                $"Scale Factor: {DisplayScaleFactor}";
+
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "Xamarin_GetDeviceScreensize",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+                            Text = ScreenDetails
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+    }
+}
